0331b5c16b7fea328569064da7c93cd4d4d7ff35
dhaifan999 <dhaifan25@gmail.com>
2024-06-13 10:25:41 +0700

    Update weapon_api.dart

--- lib/model/api/weapon_api.dart
+++ lib/model/api/weapon_api.dart
@@ -4,7 +4,7 @@ import 'package:weapon_classification/model/weapon_model.dart';
 class WeaponAPI {
   static Future<List<Weapon>> getAllDiseases() async {
     final response = await Dio().get(
-        'https://my-json-server.typicode.com/dhaifan999/mini-project-alterra/disease');
+        'https://my-json-server.typicode.com/dhaifan999/weapon_classification/weapon');
 
     List<Weapon> weapons = (response.data as List)
         .map(
